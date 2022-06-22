7a46760d4522e65ccf58189107f537a4710d76e1
mazerlodge <mazerlodge@hotmail.com>
2022-06-21 21:48:15 -0500

    Shell scripts demo checking words in dictionary and doing additions.

---
+++ ShellTools/checkadds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+dict.py -os mac -action search -searchtype word -target AUNTY >  checkresult.txt
+dict.py -os mac -action search -searchtype word -target CARON >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target FAKES >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target FEWER >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target FLEER >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target HATER >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target HOYLE >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target LONER >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target RETRY >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target SHARD >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target SHUNS >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target STEMS >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target NORTH >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target SWILL >>  checkresult.txt
+dict.py -os mac -action search -searchtype word -target TIMER >>  checkresult.txt
---
+++ ShellTools/doadds.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dict.py -os mac -action addword  -target FAKES >> addresult.txt
+dict.py -os mac -action addword  -target FEWER >> addresult.txt
+dict.py -os mac -action addword  -target FLEER >> addresult.txt
+dict.py -os mac -action addword  -target HATER >> addresult.txt
+dict.py -os mac -action addword  -target HOYLE >> addresult.txt
+dict.py -os mac -action addword  -target LONER >> addresult.txt
+dict.py -os mac -action addword  -target RETRY >> addresult.txt
+dict.py -os mac -action addword  -target SHARD >> addresult.txt
+dict.py -os mac -action addword  -target SHUNS >> addresult.txt
+dict.py -os mac -action addword  -target STEMS >> addresult.txt
+dict.py -os mac -action addword  -target NORTH >> addresult.txt
+dict.py -os mac -action addword  -target SWILL >> addresult.txt
+dict.py -os mac -action addword  -target TIMER >> addresult.txt
